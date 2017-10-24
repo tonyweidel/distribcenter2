30ecb67d6defb2d2100a7e0108c86ff6f191e0d5
Tony Weidel <8aweidel@stpatrick.org>
2017-10-24 14:42:34 -0500

    BAM BOOM POW

--- DistributionCenter.js
+++ DistributionCenter.js
@@ -13,15 +13,28 @@ function configureTruckFleet(numberoftrucks){
     let theFleet = new Queue();
     //fill the fleet with different sized trucks using math.random()
     for(let x=0;x<numberoftrucks;x++){
-      Warehouse.enqueue(new Truck());
+      TruckFleet.enqueue(new Truck());
     }
     return theFleet;
 }
 function distribute(wh,tf){
     //distribute the products in the warehouse to the truck fleet.
+    let deploy = new Queue();
+    while(!wh.isEmpty() && !tf.isEmpty()){
+    let currentTruck = tf.dequeue();
+    while(currentTruck.spaceEfficency()<1){
+      currentTruck.inventory.push(Warehouse.dequeue);
+    }
+    if(currentTruck.spaceEfficency()>1){
+      Warehouse.enqueue(currentTruck.inventory.pop());
+    }
+    deploy.enqueue(currentTruck);
+  }
+  return deploy;
 }
 function ship(fleet,percent){
     //if the trucks spaceEfficency is greater than or equal to the percent, then remove the truck from the fleet's linked list
+
 }
 function main(){
     let flemhouse = configureWareHouse();
